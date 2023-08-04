f343776d551a77eca21299df1b8dd14a31cacd31
Evan Williams <edw@evanwilliams.info>
2023-08-04 12:48:58 -0400

    fixing typos

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,9 +25,9 @@ const IndexPage = () => {
         <li>I worked as a Lead Front-end developer for a small team within a suite of tools created for GE's Engine lines for 2 years.</li>
         <li>I used agile and worked in 1-2 week sprints, designing and developing custom applications for GE's engine line. </li>
         <li>I worked in a cross-functional team with specialized roles and worked with Project Managers and UX Designers to build a product month by month to changing work requirements</li>
-        <li>I worked as part of a pipeline of Developers, QA Engineers and Scrum Masters to build applications used by General Electric employees both on shopfloors and within GE's logistics and fulfillment teams.</li>
-        <li>I created novel soutions to existing problems that were central to legacy systems and processes.</li>
-        <li>Built and deployed code to a cloud environemnt sucessfully and accepted by product owners more than 50 consecutive times in my tenure at GE.</li>
+        <li>I worked as part of a pipeline of Developers, QA Engineers and Scrum Masters to build applications used by General Electric employees both on shop floors and within GE's logistics and fulfillment teams.</li>
+        <li>I created novel solutions to existing problems that were central to legacy systems and processes.</li>
+        <li>Built and deployed code to a cloud environment successfully and accepted by product owners more than 50 consecutive times in my tenure at GE.</li>
       </ul>
 
       <h3>Worked on design systems for Hagerty and site refresh</h3>
@@ -40,9 +40,9 @@ const IndexPage = () => {
       </ul>
         <h3>Let me build you a website!</h3>
         <ul>
-          <li>I have experience building component based applications using React and Polymer.js as well as work with Content Management Systems using Wordpress, Sitecore, and Demandware/SFCC.</li>
+          <li>I have experience building component based applications using React and Polymer.js as well as work with Content Management Systems using WordPress, Sitecore, and Demandware/SFCC.</li>
           <li>I also maintain the Bayport Fish Company Website! www.bayportfish.com, Are you looking for someone to build you a website?</li>
-          <li>If you need a custom website and you want someone to build it for you and offer maintainance on a yearly scale contact me at evan@welltrek.com</li>
+          <li>If you need a custom website and you want someone to build it for you and offer maintenance on a yearly scale contact me at evan@welltrek.com</li>
         </ul>
     </div>
   )
